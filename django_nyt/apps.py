7c2f4171b77b8e77a054fd39cefd1bec2ddccbbc
Neil Osborne <neil.osborne@gmail.com>
2022-08-08 15:37:34 +0100

    (dev): Added default_auto_field='django.db.models.BigAutoField' to DyangoNytConfig

--- django_nyt/apps.py
+++ django_nyt/apps.py
@@ -5,3 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class DjangoNytConfig(AppConfig):
     name = "django_nyt"
     verbose_name = _("Django Nyt")
+    default_auto_field = 'django.db.models.BigAutoField'
